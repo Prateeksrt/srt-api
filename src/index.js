ee657b92fe5793445e1270c0cf53f8279263f1ed
Prateek Srivastava <prateeksrt2009@gmail.com>
2022-01-19 19:52:12 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 5500;
+
+app.get("/", (req, res) => {
+  res.send({ name: "Prateek", age: 32 });
+});
+
+app.get("/add", (req, res) => {
+  res.send({ name: "Prateek", age: 32, add: "Addition" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
